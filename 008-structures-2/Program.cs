59bb5ca1f3ea1b6b7ebb901b4dde590c7b43fa81
Dimitrey1978 <Dimitrey@mail.ru>
2021-04-04 22:04:45 +0300

    Добавьте файлы проекта.

---
+++ 008-structures-2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _008_structures_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Какую строку вывести?");
+            string stroka = Console.ReadLine();
+            Console.WriteLine("Каким цветом?");
+            Console.WriteLine("0 - красным");
+            Console.WriteLine("1 - голубым");
+            Console.WriteLine("2 - зеленым");
+            int color = Convert.ToInt32(Console.ReadLine());
+            PrintColor.Print(stroka, color);
+            
+        }
+    }
+    public enum ColorText { red, blue, green }
+    static class PrintColor
+    {
+        public static void Print(string stroka, int color)
+        {
+            if (color >=0&&color<=2)
+            { 
+            switch (color)
+            { 
+                case 0:            
+                Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+            }
+            }
+            else
+                Console.WriteLine("неверный номер цвета");
+
+            Console.WriteLine(stroka);
+            Console.ResetColor();
+
+           
+                
+
+
+        }
+
+        
+    }
+}
